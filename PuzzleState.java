002b9d91e2a79c69461c8486b921d4cc4e25ceb2
Reigningsun <38302744+Reigningsun@users.noreply.github.com>
2018-06-08 15:39:17 -0500

    Update PuzzleState.java
    Reformatting for Github

--- PuzzleState.java
+++ PuzzleState.java
@@ -3,17 +3,17 @@ package csc380;
 
 public class PuzzleState implements Comparable<PuzzleState> {
 
-	String tilePos;																			// order of the tiles
-	PuzzleState parent;																		// previous PuzzleState
-	int costOfPath;																			// cumulative cost of path leading to this node
-	String moveToState;																		// move that lead to this state
-	int depth;																				// depth in tree
-	short tilesOutPos;																		// stores total number of tiles not in goal position
-	short manhattanDist;																	// stores total Manhattan distances of tiles
-	short sortStyle;																		// 1: BestFirst, 2: A*, 3: A*2
+	String tilePos;													// order of the tiles
+	PuzzleState parent;												// previous PuzzleState
+	int costOfPath;													// cumulative cost of path leading to this node
+	String moveToState;												// move that lead to this state
+	int depth;													// depth in tree
+	short tilesOutPos;												// stores total number of tiles not in goal position
+	short manhattanDist;												// stores total Manhattan distances of tiles
+	short sortStyle;												// 1: BestFirst, 2: A*, 3: A*2
 	
 	
-	PuzzleState(String tilePos, PuzzleState parent, int costOfPath, String moveToState){	// constructor
+	PuzzleState(String tilePos, PuzzleState parent, int costOfPath, String moveToState){				// constructor
 		this.tilePos 		= tilePos;
 		this.parent 		= parent;
 		this.costOfPath 	= costOfPath;
@@ -23,10 +23,10 @@ public class PuzzleState implements Comparable<PuzzleState> {
 			this.depth = 0;
 		}
 		else{
-			this.depth		= parent.depth +1;
+			this.depth	= parent.depth +1;
 		}
 		
-		sortStyle 			= 0;
+		sortStyle 		= 0;
 		tilesOutPos 		= 0;
 		this.manhattanDist 	= 0;
 	}
@@ -34,20 +34,20 @@ public class PuzzleState implements Comparable<PuzzleState> {
 
 		@Override
 		public int compareTo(PuzzleState puzzle) {
-			if (sortStyle == 3){															// Astar2: comparable by cost of path + Manhattan distances + tilesOutOfPos
+			if (sortStyle == 3){										// Astar2: comparable by cost of path + Manhattan distances + tilesOutOfPos
 				return ((this.costOfPath + this.manhattanDist + this.tilesOutPos)
 						- (puzzle.costOfPath + puzzle.manhattanDist + puzzle.tilesOutPos));
 				
 			}
-			if (sortStyle == 2){															// Astar: comparable by cost of path + tiles out of position
+			if (sortStyle == 2){										// Astar: comparable by cost of path + tiles out of position
 				return ((this.costOfPath + this.tilesOutPos)
 						- (puzzle.costOfPath + puzzle.tilesOutPos));
 			}
 			
-			if (sortStyle == 1){															// BestFirst: comparable by tilesOutOfPos
+			if (sortStyle == 1){										// BestFirst: comparable by tilesOutOfPos
 				return (this.tilesOutPos - puzzle.tilesOutPos);
 			}
 			
-			return (this.costOfPath - puzzle.costOfPath);									// Default: comparable by cost of path
+			return (this.costOfPath - puzzle.costOfPath);							// Default: comparable by cost of path
 		}
 	}
